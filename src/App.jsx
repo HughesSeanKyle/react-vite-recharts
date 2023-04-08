dc9618a87299f2b0013d06482aeb81a765880fda
HughesSeanKyle <khughessean@yahoo.com>
2023-04-09 01:56:07 +0200

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import axios from 'axios';
-// * Add
 import moment from 'moment';
 import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 	const [bitcoinData, setBitcoinData] = useState([]);
-	// * Add
 	const [preparedData, setPreparedData] = useState([]);
 
 	useEffect(() => {
@@ -17,16 +15,13 @@ function App() {
 			.then((response) => {
 				setBitcoinData(response.data);
 				console.log('bitcoinData', bitcoinData);
-				// * Add
 				const prepareData = response.data.prices.map((dataPoint) => {
 					return {
 						timestamp: moment(dataPoint[0]).format('M/D/YYYY h:mm A'),
 						price: dataPoint[1],
 					};
 				});
-				// * Add
 				setPreparedData(prepareData);
-				// * Add
 				console.log('preparedData', preparedData);
 			})
 			.catch((error) => {
