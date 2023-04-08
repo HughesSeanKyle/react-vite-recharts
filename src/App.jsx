0eca44028bedcdfbb3324a77557d7038f992fe6e
HughesSeanKyle <khughessean@yahoo.com>
2023-04-09 01:49:11 +0200

    Format timestamps in response data prices array

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-// *Add
 import axios from 'axios';
+// * Add
+import moment from 'moment';
 import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-	// * Add
 	const [bitcoinData, setBitcoinData] = useState([]);
-
 	// * Add
+	const [preparedData, setPreparedData] = useState([]);
+
 	useEffect(() => {
 		axios
 			.get(
@@ -16,6 +17,17 @@ function App() {
 			.then((response) => {
 				setBitcoinData(response.data);
 				console.log('bitcoinData', bitcoinData);
+				// * Add
+				const prepareData = response.data.prices.map((dataPoint) => {
+					return {
+						timestamp: moment(dataPoint[0]).format('M/D/YYYY h:mm A'),
+						price: dataPoint[1],
+					};
+				});
+				// * Add
+				setPreparedData(prepareData);
+				// * Add
+				console.log('preparedData', preparedData);
 			})
 			.catch((error) => {
 				console.log(error);
