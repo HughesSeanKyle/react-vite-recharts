1d61e26c642f9122528903abdb8501cca2735917
HughesSeanKyle <khughessean@yahoo.com>
2023-04-08 16:41:02 +0200

    axios logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+// *Add
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+	// * Add
+	const [bitcoinData, setBitcoinData] = useState([]);
+
+	// * Add
+	useEffect(() => {
+		axios
+			.get(
+				'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1'
+			)
+			.then((response) => {
+				setBitcoinData(response.data);
+				console.log('bitcoinData', bitcoinData);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
+
 	return;
 }
 
